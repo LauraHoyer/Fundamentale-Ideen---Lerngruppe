e1151d46bc7aa3103900949450e7c999bff9b846
LauraHoyer <l.hoyer@web.de>
2015-02-04 12:51:42 +0100

    test von rubyMine aus

---
+++ einfache_schleife.rb
@@ -0,0 +1,19 @@
+zahlBeginn = 0
+
+loop do
+  zahlerhöhen = zahlBeginn += 1
+  puts "Variante 1: #{zahlerhöhen}"
+  break if zahlBeginn >= 100
+end
+
+puts "-------------------------------------------
+andere Schleife, gleiches Ergebis
+-------------------------------------------"
+
+
+a = 0
+
+while a < 100
+  a += 1
+  puts "Variante 2: #{a}"
+end
