4187b85778ed70a2cf7e0913366f33be6131c7a9
HannaKorn <hanna.korn@web.de>
2015-02-09 11:11:56 +0100

    Update einfache_schleife.rb
    Habe variante 0 geändert

--- einfache_schleife.rb
+++ einfache_schleife.rb
@@ -2,7 +2,7 @@ zahlBeginn = 0
 #Änderung eines Kommentars
 loop do
   zahlerhöhen = zahlBeginn += 1
-  puts "Variante 1: #{zahlerhöhen}"
+  puts "variante 0: #{zahlerhöhen}"
   break if zahlBeginn >= 100
 end
 
